62b7dbc977def341e7b7a7c2677df9a23adcfe47
jack <jack@jack-ThinkPad-W541>
2018-04-18 11:13:50 +0800

    test

---
+++ main.cc
@@ -0,0 +1,18 @@
+/*
+ * main.cc
+ *
+ *  Created on: 2018年4月18日
+ *      Author: jack
+ */
+
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string s("hello");
+  std::cout << "Test: " << s << std::endl;
+
+  return 0;
+}
+
+
